334a4c1f9ca267b5cce0148ba3d6af1fa38775e3
Anthony Vu <70167429+vua11@users.noreply.github.com>
2022-09-12 20:41:56 -0400

    Create koko-eating-bananas.py

---
+++ koko-eating-bananas.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        k = 0
+        while l <= r:
+            m = (l + r) // 2
+            time = 0
+            for p in piles:
+                time += ((p - 1) // m) + 1
+            if time <= h:
+                k = m
+                r = m - 1
+            else:
+                l = m + 1
+        return k
